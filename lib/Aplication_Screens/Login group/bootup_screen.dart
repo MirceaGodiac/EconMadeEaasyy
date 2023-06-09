0300e202e44591ddd82ac72a4cfdf5916090c72b
mircea <mircea.godiac@gmail.com>
2023-06-09 21:15:30 +0300

    ui doodle backgroud 1

--- lib/Aplication_Screens/Login group/bootup_screen.dart
+++ lib/Aplication_Screens/Login group/bootup_screen.dart
@@ -1,3 +1,4 @@
+import 'package:econ_made_easy_files/Aplication_Screens/Login%20group/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
 class InfoScreen1 extends StatelessWidget {
@@ -8,20 +9,20 @@ class InfoScreen1 extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          height: 400,
+          height: 300,
           child: Image.asset(
             'lib/images/Illustration34_1.png',
             filterQuality: FilterQuality.medium,
           ),
         ),
-        Text(
+        const Text(
           'Ai nevoie de ajutor la mate pentru Evaluarea Nationala?',
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.w400,
           ),
         ),
-        Text(
+        const Text(
           'Ai venit unde trebuie!',
           style: TextStyle(
             fontSize: 25,
@@ -41,20 +42,20 @@ class InfoScreen2 extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          height: 400,
+          height: 300,
           child: Image.asset(
             'lib/images/Illustration37.png',
             filterQuality: FilterQuality.medium,
           ),
         ),
-        Text(
+        const Text(
           'Zeci de modele de teste si multe resurse de incredere',
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.w400,
           ),
         ),
-        Text(
+        const Text(
           'Pentru a te ajuta la examen.',
           style: TextStyle(
             fontSize: 25,
@@ -74,21 +75,54 @@ class InfoScreen3 extends StatelessWidget {
     return Column(
       children: [
         SizedBox(
-          height: 400,
+          height: 300,
           child: Image.asset(
             'lib/images/Duplicat_2.png',
             filterQuality: FilterQuality.medium,
           ),
         ),
-        Text(
+        const Text(
           'Teste personalizate pentru orice exercitii de la EN',
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.w400,
           ),
         ),
-        Text(
+        const Text(
           'Cu recompense pe masura.',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w400,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class InfoScreen4 extends StatelessWidget {
+  const InfoScreen4({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 300,
+          child: Image.asset(
+            'lib/images/Duplicat.png',
+            filterQuality: FilterQuality.medium,
+          ),
+        ),
+        const Text(
+          'Orar personalizat pentru organizarea studiilor',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+        const Text(
+          'Pentru a te ajuta cu invatatul.',
           style: TextStyle(
             fontSize: 25,
             fontWeight: FontWeight.w400,
@@ -109,13 +143,23 @@ class BootupScreen extends StatefulWidget {
 class _BootupScreenState extends State<BootupScreen>
     with TickerProviderStateMixin {
   static int pageIndex = 0;
-  var pages = [
+  var pages = const [
     InfoScreen1(),
     InfoScreen2(),
     InfoScreen3(),
+    InfoScreen4(),
   ];
   late AnimationController controller;
+
+  final pageController = PageController(initialPage: 0);
   @override
+  void dispose() {
+    pageController.dispose();
+    controller.dispose();
+
+    super.dispose();
+  }
+
   void initState() {
     controller = AnimationController(
       /// [AnimationController]s can be created with `vsync: this` because of
@@ -124,9 +168,20 @@ class _BootupScreenState extends State<BootupScreen>
       duration: const Duration(seconds: 5),
       animationBehavior: AnimationBehavior.values.last,
     )..addListener(() {
-        setState(() {});
-        print('yes.');
+        setState(() {
+          if (controller.value >= 0.99) {
+            debugPrint('hello');
+            if (pageIndex < 3) {
+              pageIndex++;
+              pageController.jumpToPage(pageIndex);
+            } else {
+              pageIndex = 0;
+              pageController.jumpToPage(pageIndex);
+            }
+          }
+        });
       });
+
     controller.repeat(reverse: false);
 
     super.initState();
@@ -134,33 +189,107 @@ class _BootupScreenState extends State<BootupScreen>
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white60,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(
-            height: 20,
+      body: Stack(children: [
+        SizedBox(
+          height: double.infinity,
+          child: Image.asset(
+            'lib/images/logindoodle.jpg',
+            filterQuality: FilterQuality.medium,
+            fit: BoxFit.cover,
           ),
-          Center(
-            child: Text(
-              'E N  A B L E  ',
-              style: TextStyle(fontSize: 80, fontWeight: FontWeight.w100),
+        ),
+        Container(
+          height: double.infinity,
+          margin: const EdgeInsets.only(top: 30, left: 280, right: 280),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(60),
+              topRight: Radius.circular(60),
             ),
           ),
-          pages[pageIndex],
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 250),
-            child: LinearProgressIndicator(
-              value: controller.value,
-              semanticsLabel: 'Linear progress indicator',
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'E N',
+                    style: TextStyle(
+                      fontSize: 82,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Text(
+                    ' A B L E  ',
+                    style: TextStyle(fontSize: 80, fontWeight: FontWeight.w100),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 400,
+                child: PageView(
+                  controller: pageController,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [
+                    InfoScreen1(),
+                    InfoScreen2(),
+                    InfoScreen3(),
+                    InfoScreen4(),
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 250),
+                child: LinearProgressIndicator(
+                  backgroundColor: Colors.white,
+                  color: Colors.blue.shade200,
+                  minHeight: 20,
+                  value: controller.value,
+                  semanticsLabel: 'Linear progress indicator',
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return WelcomePage();
+                    },
+                  ));
+                },
+                child: Container(
+                  width: 400,
+                  height: 60,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      color: Colors.blue),
+                  child: const Center(
+                    child: Text(
+                      'Hai sa incepem!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          const SizedBox(
-            height: 40,
-          )
-        ],
-      ),
+        ),
+      ]),
     );
   }
 }
--- lib/Aplication_Screens/Login group/welcome_screen.dart
+++ lib/Aplication_Screens/Login group/welcome_screen.dart
@@ -14,132 +14,168 @@ class _WelcomePageState extends State<WelcomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Container(
-                      margin: const EdgeInsets.only(top: 150, left: 30),
-                      child: const Text(
-                        "Bine ai Venit!",
-                        style: TextStyle(
-                          fontSize: 90,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(left: 30),
+      body: Stack(children: [
+        SizedBox(
+          height: double.infinity,
+          child: Image.asset(
+            'lib/images/logindoodle.jpg',
+            filterQuality: FilterQuality.medium,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Center(
+          child: Container(
+            width: double.infinity,
+            height: 500,
+            color: Colors.white,
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Container(
+                        margin: const EdgeInsets.only(top: 150, left: 30),
                         child: const Text(
-                          "Ai cont Econ \nMade Easy?",
+                          "Bine ai Venit!",
                           style: TextStyle(
-                            fontSize: 60,
-                            fontWeight: FontWeight.w300,
+                            fontSize: 90,
+                            fontWeight: FontWeight.w400,
                           ),
                         ),
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Container(
-                        margin: const EdgeInsets.only(left: 30),
-                        child: Column(
-                          children: [
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(context, MaterialPageRoute(
-                                  builder: (context) {
-                                    return const LoginEnterEmailPage();
-                                  },
-                                ));
-                              },
-                              child: Container(
-                                height: 50,
-                                width: 300,
-                                decoration: const BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20)),
-                                    color: Colors.blue),
-                                child: const Center(
-                                  child: Text(
-                                    'Da',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(left: 30),
+                          child: const Row(
+                            children: [
+                              Text(
+                                "Ai cont ",
+                                style: TextStyle(
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.w300,
                                 ),
                               ),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            InkWell(
-                              onTap: () {
-                                Navigator.push(context, MaterialPageRoute(
-                                  builder: (context) {
-                                    return const RegisterEnterEmailPage();
-                                  },
-                                ));
-                              },
-                              child: Container(
-                                height: 50,
-                                width: 300,
-                                decoration: const BoxDecoration(
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20)),
-                                    color: Colors.blue),
-                                child: const Center(
-                                  child: Text(
-                                    'Nu',
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
+                              Text(
+                                "E N ",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.w400,
                                 ),
                               ),
-                            ),
-                          ],
+                              Text(
+                                "A B L E ?",
+                                style: TextStyle(
+                                  fontSize: 60,
+                                  fontWeight: FontWeight.w300,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 84),
-                height: 640,
-                child: Image.asset(
-                  'lib/images/welcome_avatar',
-                  filterQuality: FilterQuality.medium,
+                        const SizedBox(
+                          height: 100,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(left: 30),
+                          child: Column(
+                            children: [
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) {
+                                      return const LoginEnterEmailPage();
+                                    },
+                                  ));
+                                },
+                                child: Container(
+                                  height: 50,
+                                  width: 300,
+                                  decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.blue),
+                                  child: const Center(
+                                    child: Text(
+                                      'Da',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) {
+                                      return const RegisterEnterEmailPage();
+                                    },
+                                  ));
+                                },
+                                child: Container(
+                                  height: 50,
+                                  width: 300,
+                                  decoration: const BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(20)),
+                                      color: Colors.blue),
+                                  child: const Center(
+                                    child: Text(
+                                      'Nu',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              )
-            ],
-          ),
-          Container(
-            alignment: Alignment.bottomRight,
-            child: Text("Econ Made Easy v1.1, 2023"),
-          )
-        ],
-      ),
+                Container(
+                  margin: const EdgeInsets.only(top: 84),
+                  height: 640,
+                  child: Image.asset(
+                    'lib/images/welcome_avatar',
+                    filterQuality: FilterQuality.medium,
+                  ),
+                )
+              ],
+            ),
+            Container(
+              alignment: Alignment.bottomRight,
+              child: Text("Econ Made Easy v1.1, 2023"),
+            )
+          ],
+        ),
+      ]),
     );
   }
 }
